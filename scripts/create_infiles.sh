072b69ab7eb1888394e2b8e99232117494ce522d
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-04-06 19:54:15 +0300

    generate random string

--- scripts/create_infiles.sh
+++ scripts/create_infiles.sh
@@ -9,7 +9,7 @@ validate_args() {
                 (>&2 echo "Error: Invalid arguments!")
                 exit 1
             fi
-            shift +1
+            shift
             done
     else
         (>&2 echo "Error: Too few arguments!")
@@ -23,7 +23,24 @@ echo "Number of arguments: [$#]"
 
 validate_args $@
 
-mkdir $1; cd $1;
+# Random characters.
+x="abcdefghijklwricoalc234019495023";
+
+# Make input_dir recursively.
+mkdir -p $1;
+
+ch="";
+if [[ $? -eq 0 ]]; then
+    cd $1;
+	strlen=`expr length "$x"`;
+    characters=`expr $RANDOM % 8 + 1`;
+    for ((i=0; i <= $characters; i++)) do
+        c=`expr $RANDOM % $strlen + 1`;
+        ch+=`expr substr $x $c 1`;
+        echo "$i: $ch";
+    done
+    echo $ch
+fi
 
 
 #echo $(($RANDOM % 2))
