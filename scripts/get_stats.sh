fc79bc2a4f512b26c5173152f1d9f95dd46ffbc1
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-04-08 03:07:48 +0300

    get_stats.sh read from stdin, switch commands

--- scripts/get_stats.sh
+++ scripts/get_stats.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+while read line; do
+    cmd=`echo ${line} | cut -d' ' -f1`
+    value=`echo ${line} | cut -d' ' -f2`
+
+    case "${cmd}" in
+
+    cl) echo "cl command ${value}"
+
+        ;;
+    bs) echo "bs command ${value}"
+
+        ;;
+    br) echo "br command ${value}"
+
+        ;;
+    fs) echo "fs command ${value}"
+
+        ;;
+    fr) echo "fr command ${value}"
+
+        ;;
+    dn) echo "dn command ${value}"
+
+        ;;
+    esac
+
+done < /dev/stdin
