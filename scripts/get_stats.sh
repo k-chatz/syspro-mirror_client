e9fdf1c6da8d93835061bf29339042106e0874d4
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-04-08 03:29:20 +0300

    get_stats.sh declare vars, counters

--- scripts/get_stats.sh
+++ scripts/get_stats.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 
+clients=0
+bs=0
+br=0
+fs=0
+fr=0
+dn=0
+
 while read line; do
     cmd=`echo ${line} | cut -d' ' -f1`
     value=`echo ${line} | cut -d' ' -f2`
 
     case "${cmd}" in
 
-    cl) echo "cl command ${value}"
-
+    cl)
+        ((clients++))
+        #todo: add client in array
         ;;
-    bs) echo "bs command ${value}"
-
+    bs)
+        ((bs+=value))
         ;;
-    br) echo "br command ${value}"
-
+    br)
+        ((br+=value))
         ;;
-    fs) echo "fs command ${value}"
-
+    fs)
+        ((fs+=value))
         ;;
-    fr) echo "fr command ${value}"
-
+    fr)
+        ((fr+=value))
         ;;
-    dn) echo "dn command ${value}"
-
+    dn)
+        ((dn++))
         ;;
     esac
 
 done < /dev/stdin
+
+echo "Clients: ${clients}"
+echo "Minimum client id: ${clients}" #todo: find min value from array
+echo "Maximum client id: ${clients}" #todo: find max value from array
+echo "Sent bytes: ${bs}"
+echo "Receive bytes: ${br}"
+echo "Sent files: ${fs}"
+echo "Receive files: ${fr}"
+echo "Clients who leave: ${dn}"
